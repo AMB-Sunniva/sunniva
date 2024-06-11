8c4decf1590c9bbcdf0b45ccea4f402357e042d8
Kajsia Anderson <kajsia.anderson@gmail.com>
2024-06-10 20:16:24 -0600

    modified header and home page

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
 import "./globals.css";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function Layout({ children }) {
   return (
@@ -7,23 +9,12 @@ export default function Layout({ children }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        <title>My Site</title>
+        <title>Sunniva</title>
       </head>
       <body>
-        <header>
-          <nav>
-            <a href="/">Home</a>
-            <a href="/what-we-offer">What We Offer</a>
-            <a href="/why-sunniva">Why Sunniva</a>
-            <a href="/installer">Installers</a>
-            <a href="/shop">Shop</a>
-            <a href="/contact-us">Contact Us</a>
-          </nav>
-        </header>
+        <Header />
         <main>{children}</main>
-        <footer>
-          <p>© 2023 My Site</p>
-        </footer>
+        <Footer />
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
+import LandingPage from "@/components/LandingPage";
+
 export default function HomePage() {
   return (
     <div>
+      <LandingPage />
       <h1>Home Page</h1>
       <p>Welcome to the home page!</p>
     </div>
