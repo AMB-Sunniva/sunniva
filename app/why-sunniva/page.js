1d21f3849aadedc7a9a17d6bcf4104d16c8a736c
Kajsia Anderson <kajsia.anderson@gmail.com>
2024-06-11 16:04:55 -0600

    fixed single quotes

--- app/why-sunniva/page.js
+++ app/why-sunniva/page.js
@@ -9,7 +9,7 @@ export default function WhySunnivaPage() {
             Choose Sunniva as your solar and pergola installer, and unlock a world of benefits that go beyond expectations. By harnessing the power of solar energy, our installations bring not only substantial savings on energy bills but also an undeniable increase in property value. We offer flexible financing options that make transitioning to renewable energy hassle-free, while also guiding you through tax credits to maximize your investment.
         </p>
         <p>
-            Our commitment to customizability ensures that your solar shade structure perfectly complements your unique aesthetic preferences and outdoor space. Experience the dual purpose of our installations, providing both shade and clean energy generation, all while seamlessly preserving the integrity of your roof. With us, you're not just investing in a structure; you're embracing a greener future, enhancing your property's appeal, and enjoying the unmatched charm of a personalized outdoor sanctuary.
+            Our commitment to customizability ensures that your solar shade structure perfectly complements your unique aesthetic preferences and outdoor space. Experience the dual purpose of our installations, providing both shade and clean energy generation, all while seamlessly preserving the integrity of your roof. With us, you&apos;re not just investing in a structure; you&apos;re embracing a greener future, enhancing your property&apos;s appeal, and enjoying the unmatched charm of a personalized outdoor sanctuary.
         </p>
 
         <div className={styles.container}>
@@ -17,7 +17,7 @@ export default function WhySunnivaPage() {
                 <div className={styles.content}>
                     <h2>OUR STORY</h2>
                     <p>
-                        Driven by an unwavering passion, we are committed to enriching people's lives by delivering exceptional products that seamlessly blend innovation, aesthetics, and functionality.
+                        Driven by an unwavering passion, we are committed to enriching people&apos;s lives by delivering exceptional products that seamlessly blend innovation, aesthetics, and functionality.
                         <br/>
                         <br/>
                         We are a passionate team dedicated to reimagining outdoor spaces through the synergy of sustainability and creativity. Our mission is to bring the beauty of nature and the power of renewable energy together, crafting exceptional structures that not only enhance lives but also contribute positively to the environment.
