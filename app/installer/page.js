5e1f882a91958b587d021024915e564f3d87f7ad
Kajsia Anderson <kajsia.anderson@gmail.com>
2024-06-12 17:55:40 -0600

    fixed apostrophe

--- app/installer/page.js
+++ app/installer/page.js
@@ -17,12 +17,12 @@ export default function Installer() {
           <div className={styles.cardContent}>
             <div className={styles.benefits}>
               <h2 className={styles.benefitsTitle}>BENEFITS</h2>
-              <p className={styles.benefitsDescription}>Our Installer Discount offers installers a multitude of benefits, including discounted pricing to enhance profitability and save on project costs. By purchasing multiple systems, installers can also enjoy reduced shipping expenses, further maximizing their savings and improving their bottom line. Additionally, offering a wider range of high-quality solar products not only increases installer credibility and customer satisfaction but also fosters greater opportunities for positive reviews and referrals. Join our program today and be a part of making dreams come true for homeowners while boosting your business's profitability and enhancing the lives of families in your community.</p>
+              <p className={styles.benefitsDescription}>Our Installer Discount offers installers a multitude of benefits, including discounted pricing to enhance profitability and save on project costs. By purchasing multiple systems, installers can also enjoy reduced shipping expenses, further maximizing their savings and improving their bottom line. Additionally, offering a wider range of high-quality solar products not only increases installer credibility and customer satisfaction but also fosters greater opportunities for positive reviews and referrals. Join our program today and be a part of making dreams come true for homeowners while boosting your business&apos;s profitability and enhancing the lives of families in your community.</p>
             </div>
             <hr style={{width:'10%', borderColor: '#333', margin: '16px 32px 16px 50%'}} />
             <div className={styles.howContent}>
               <h2 className={styles.howTitle}>HOW IT WORKS</h2>
-              <p className={styles.howDescription}>Our installer program operates seamlessly: simply sign up below to receive your exclusive login credentials from Sunniva Solar. Once logged in, you'll gain access to our installer-only product page, where you can browse and select the systems you wish to purchase. At checkout, apply your company-specific coupon code to unlock your discounted pricing, or reach out to our office directly for personalized assistance and guidance.</p>
+              <p className={styles.howDescription}>Our installer program operates seamlessly: simply sign up below to receive your exclusive login credentials from Sunniva Solar. Once logged in, you&apos;ll gain access to our installer-only product page, where you can browse and select the systems you wish to purchase. At checkout, apply your company-specific coupon code to unlock your discounted pricing, or reach out to our office directly for personalized assistance and guidance.</p>
             </div>
           </div>
         </div>
