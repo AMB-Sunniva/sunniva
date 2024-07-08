fddb6f396034a0e38f9f511ac7b5cda504348675
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2024-07-08 04:25:21 -0600

    Temp

--- app/api/stripe/webhook/route.js
+++ app/api/stripe/webhook/route.js
@@ -1,47 +1,47 @@
-import { NextResponse } from "next/server";
-import { buffer } from "micro";
-import Stripe from "stripe";
+// import { NextResponse } from "next/server";
+// import { buffer } from "micro";
+// import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-export async function POST(req) {
-  const buf = await buffer(req);
-  const sig = req.headers.get("stripe-signature");
+// export async function POST(req) {
+//   const buf = await buffer(req);
+//   const sig = req.headers.get("stripe-signature");
 
-  let event;
+//   let event;
 
-  try {
-    event = stripe.webhooks.constructEvent(
-      buf,
-      sig,
-      process.env.STRIPE_WEBHOOK_SECRET
-    );
-  } catch (err) {
-    return new NextResponse(`Webhook Error: ${err.message}`, { status: 400 });
-  }
+//   try {
+//     event = stripe.webhooks.constructEvent(
+//       buf,
+//       sig,
+//       process.env.STRIPE_WEBHOOK_SECRET
+//     );
+//   } catch (err) {
+//     return new NextResponse(`Webhook Error: ${err.message}`, { status: 400 });
+//   }
 
-  // Handle the event
-  switch (event.type) {
-    case "payment_intent.succeeded":
-      const paymentIntent = event.data.object;
-      console.log("PaymentIntent was successful!", paymentIntent);
-      // Handle successful payment intent
-      break;
-    // Handle other event types
-    default:
-      console.log(`Unhandled event type ${event.type}`);
-  }
+//   // Handle the event
+//   switch (event.type) {
+//     case "payment_intent.succeeded":
+//       const paymentIntent = event.data.object;
+//       console.log("PaymentIntent was successful!", paymentIntent);
+//       // Handle successful payment intent
+//       break;
+//     // Handle other event types
+//     default:
+//       console.log(`Unhandled event type ${event.type}`);
+//   }
 
-  return new NextResponse(JSON.stringify({ received: true }), { status: 200 });
-}
+//   return new NextResponse(JSON.stringify({ received: true }), { status: 200 });
+// }
 
-// Update the configuration format
-export const dynamic = "force-static";
-export const runtime = "nodejs";
-export const preferredRegion = "auto";
+// // Update the configuration format
+// export const dynamic = "force-static";
+// export const runtime = "nodejs";
+// export const preferredRegion = "auto";
 
-export const config = {
-  api: {
-    bodyParser: false, // Disallow body parsing, consume as stream
-  },
-};
+// export const config = {
+//   api: {
+//     bodyParser: false, // Disallow body parsing, consume as stream
+//   },
+// };
