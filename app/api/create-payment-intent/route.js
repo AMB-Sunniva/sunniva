ba956932a1fc7a3ac0f50435e892ba3e58df18cc
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2024-07-17 23:15:24 -0600

    tests

--- app/api/create-payment-intent/route.js
+++ app/api/create-payment-intent/route.js
@@ -3,7 +3,9 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export async function POST(request) {
   try {
+    console.log("Request received");
     const { amount } = await request.json();
+    console.log("Amount:", amount);
 
     const paymentIntent = await stripe.paymentIntents.create({
       amount: amount,
@@ -11,11 +13,13 @@ export async function POST(request) {
       automatic_payment_methods: { enabled: true },
     });
 
+    console.log("PaymentIntent created:", paymentIntent);
+
     return NextResponse.json({ clientSecret: paymentIntent.client_secret });
   } catch (error) {
-    console.error("Internal Error:", error);
+    console.error("Internal Error:", error.message);
     return NextResponse.json(
-      { error: `Internal Server Error: ${error}` },
+      { error: `Internal Server Error: ${error.message}` },
       { status: 500 }
     );
   }
