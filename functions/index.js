a147465222dd24ce20ba328a0cdebb703038620f
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2024-07-19 21:27:28 -0600

    Added cors

--- functions/index.js
+++ functions/index.js
@@ -11,27 +11,32 @@
 // const logger = require("firebase-functions/logger");
 
 const functions = require("firebase-functions");
+const cors = require("cors")({ origin: true });
 const stripe = require("stripe")(functions.config().stripe.secret);
 
-exports.createPaymentIntent = functions.https.onRequest(async (req, res) => {
-  if (req.method !== "POST") {
-    return res.status(405).send("Method Not Allowed");
-  }
+exports.createPaymentIntent = functions.https.onRequest((req, res) => {
+  cors(req, res, async () => {
+    if (req.method !== "POST") {
+      return res.status(405).send("Method Not Allowed");
+    }
 
-  try {
-    const { amount } = req.body;
+    try {
+      const { amount } = req.body;
 
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: amount,
-      currency: "usd",
-      automatic_payment_methods: { enabled: true },
-    });
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        automatic_payment_methods: { enabled: true },
+      });
 
-    res.status(200).json({ clientSecret: paymentIntent.client_secret });
-  } catch (error) {
-    console.error("Error creating payment intent:", error);
-    res.status(500).json({ error: `Internal Server Error: ${error.message}` });
-  }
+      res.status(200).json({ clientSecret: paymentIntent.client_secret });
+    } catch (error) {
+      console.error("Error creating payment intent:", error);
+      res
+        .status(500)
+        .json({ error: `Internal Server Error: ${error.message}` });
+    }
+  });
 });
 
 // Create and deploy your first functions
