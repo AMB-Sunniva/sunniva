58d25ffbfe75e6d67dfaeecc680692da0348bb10
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2024-11-08 23:07:24 -0700

    Sidebar added

--- app/shop/page.js
+++ app/shop/page.js
@@ -14,6 +14,7 @@ import Button from "@/components/Button";
 export default function Shop() {
   const { products, loading } = useFirestore();
   const [selectedCategory, setSelectedCategory] = useState(null);
+  const [isSidebarHidden, setIsSidebarHidden] = useState(false);
   const [isSidebarVisible, setIsSidebarVisible] = useState(true);
 
   if (loading) return <div>Loading...</div>;
@@ -24,107 +25,115 @@ export default function Shop() {
 
   const handleCategory = (category) => {
     setSelectedCategory(category);
-    setIsSidebarVisible(true); // Show sidebar when a category is selected
+    setIsSidebarVisible(true);
   };
 
   const handleBackClick = () => {
     setSelectedCategory(null);
-    setIsSidebarVisible(true); // Reset sidebar visibility when going back to categories
+    setIsSidebarVisible(false);
   };
 
   const toggleSidebar = () => {
-    setIsSidebarVisible(!isSidebarVisible);
+    setIsSidebarHidden(!isSidebarHidden);
   };
 
   return (
-    <div className="container mx-auto py-8 flex">
-      {selectedCategory && (
-        <>
-          {/* Sidebar - Visible only when a category is selected */}
+    <div className="w-full py-8 flex justify-center">
+      <div className="flex w-full">
+        {isSidebarVisible && (
           <div
             className={`${
-              isSidebarVisible ? "w-48" : "w-12"
-            } h-[85vh] border-r border-gray-300 p-2 relative transition-all duration-300`}
+              isSidebarHidden ? "w-12" : "w-48"
+            } h-[calc(100vh-4rem)] border-r border-gray-300 p-2 transition-all duration-300 sticky top-20 bg-white flex`}
           >
             <button
               onClick={toggleSidebar}
               className="absolute -right-4 top-1/2 transform -translate-y-1/2 bg-gray-100 rounded-full p-1 shadow-md hover:bg-gray-200"
             >
-              {isSidebarVisible ? (
-                <IoIosArrowBack size="1.5em" />
-              ) : (
+              {isSidebarHidden ? (
                 <IoIosArrowForward size="1.5em" />
+              ) : (
+                <IoIosArrowBack size="1.5em" />
               )}
             </button>
-            {isSidebarVisible && <ProductSideBar products={products} />}
-          </div>
-        </>
-      )}
-
-      {/* Main Content */}
-      <div className="flex-1 px-8">
-        <div
-          style={{
-            padding: "6rem 0rem 2rem",
-            textAlign: "center",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <hr
-            style={{ width: "3%", borderColor: "#333", margin: "30px auto" }}
-          />
-          {selectedCategory ? (
-            <div>
-              <h1
-                style={{
-                  fontSize: "2.5rem",
-                  fontWeight: "lighter",
-                  color: "#474949",
-                  letterSpacing: "2px",
-                  textTransform: "uppercase",
-                  paddingBottom: "32px",
-                }}
-              >
-                {selectedCategory}
-              </h1>
-              <button
-                onClick={handleBackClick}
-                type="button"
-                className="text-custom-blue bg-white hover:underline flex items-center justify-center pb-3"
-              >
-                <IoIosArrowRoundBack size="1.5em" />
-                Back to Products
-              </button>
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-                {filteredProducts.map((product) => (
-                  <ProductCard key={product.id} product={product} />
-                ))}
+            {!isSidebarHidden && (
+              <div className="flex">
+                <ProductSideBar products={products} />
               </div>
-            </div>
-          ) : (
-            <div className="flex flex-col md:flex-row md:gap-x-4 w-5/6 md:w-3/4 p-2 h-[50vh]">
-              <Button
-                type="kitButton"
-                onClick={() => handleCategory("Solar and Shade Kits")}
-              >
-                Solar And Shade
-              </Button>
-              <Button
-                type="kitButton"
-                onClick={() => handleCategory("Just Shade Kits")}
-              >
-                Just Shade
-              </Button>
-              <Button
-                type="kitButton"
-                onClick={() => handleCategory("Just Solar Kits")}
-              >
-                Just Solar
-              </Button>
-            </div>
-          )}
+            )}
+          </div>
+        )}
+
+        <div
+          className={`flex-1 px-8 transition-all duration-300 ${
+            isSidebarVisible && !isSidebarHidden ? "ml-2" : "ml-0"
+          }`}
+        >
+          <div
+            style={{
+              padding: "6rem 0rem 2rem",
+              textAlign: "center",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <hr
+              style={{ width: "3%", borderColor: "#333", margin: "30px auto" }}
+            />
+            {selectedCategory ? (
+              <>
+                <div>
+                  <h1
+                    style={{
+                      fontSize: "2.5rem",
+                      fontWeight: "lighter",
+                      color: "#474949",
+                      letterSpacing: "2px",
+                      textTransform: "uppercase",
+                      paddingBottom: "32px",
+                    }}
+                  >
+                    {selectedCategory}
+                  </h1>
+                  <button
+                    onClick={handleBackClick}
+                    type="button"
+                    className="text-custom-blue bg-white hover:underline flex items-center justify-center pb-3"
+                  >
+                    <IoIosArrowRoundBack size="1.5em" />
+                    Back to Products
+                  </button>
+                  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+                    {filteredProducts.map((product) => (
+                      <ProductCard key={product.id} product={product} />
+                    ))}
+                  </div>
+                </div>
+              </>
+            ) : (
+              <div className="flex flex-col md:flex-row md:gap-x-4 w-5/6 md:w-3/4 p-2 h-[50vh]">
+                <Button
+                  type="kitButton"
+                  onClick={() => handleCategory("Solar and Shade Kits")}
+                >
+                  Solar And Shade
+                </Button>
+                <Button
+                  type="kitButton"
+                  onClick={() => handleCategory("Just Shade Kits")}
+                >
+                  Just Shade
+                </Button>
+                <Button
+                  type="kitButton"
+                  onClick={() => handleCategory("Just Solar Kits")}
+                >
+                  Just Solar
+                </Button>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
--- components/Shop/ProductSideBar.js
+++ components/Shop/ProductSideBar.js
@@ -14,30 +14,20 @@ const ProductSideBar = ({ products }) => {
   });
 
   return (
-    <div className="w-64 p-4 bg-gray-100 border-r border-gray-300">
+    <div className="w-48 p-4 bg-gray-100 border-r border-gray-300">
       <h2 className="text-xl font-semibold mb-4">Products</h2>
-      {["Solar and Shade", "Just Solar Kits", "Just Shade Kits"].map((type) => (
-        <div key={type} className="mb-6">
-          <h3 className="text-lg font-medium text-gray-700 mb-2">{type}</h3>
-          <ul className="space-y-2">
-            {sortedProducts
-              .filter((product) => product.type === type)
-              .map((product) => (
-                <li
-                  key={product.id}
-                  className="flex justify-between items-center"
-                >
-                  <Link
-                    href={`/product/${product.id}`}
-                    className="text-blue-600 hover:underline"
-                  >
-                    {product.name}
-                  </Link>
-                </li>
-              ))}
-          </ul>
-        </div>
-      ))}
+      <ul className="space-y-2">
+        {sortedProducts.map((product) => (
+          <li key={product.id} className="flex justify-between items-center">
+            <Link
+              href={`/product/${product.id}`}
+              className="text-blue-600 hover:underline"
+            >
+              {product.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
