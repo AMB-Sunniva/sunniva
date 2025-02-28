566ac19e0c256fd87eeb4f8f645b685b44f447ac
AMB-Sunniva <amb.sunnivasol@gmail.com>
2025-02-27 23:44:40 -0700

    Update LandingPage.js

--- components/LandingPage.js
+++ components/LandingPage.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const LandingPage = () => {
   return (
-    <div style={{ position: "relative", width: "90%", height: "100vh" }}>
+    <div style={{ position: "relative", width: "100%", height: "100%" }}>
       <Image
         src="/images/EILC1335.jpg"
         alt="Solar"
